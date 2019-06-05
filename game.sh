3e56535f14289f854623fce4ccbc117a5912a263
june <abgood@sina.com>
2019-06-05 16:14:50 +0800

    init game

---
+++ game.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash 
+
+dir=$(pwd)
+map_name="crazy_monster.w3x"
+
+main(){
+	for a in $@; do
+		case $a in
+	    	run)
+				${dir}/tools/w3x2lni/bin/w3x2lni-lua.exe ${dir}/tools/运行.lua ${dir}
+	    		;;
+	    	config)
+				${dir}/tools/w3x2lni/bin/w3x2lni-lua.exe ${dir}/tools/配置.lua ${dir}
+	    		;;
+	    	check)
+				${dir}/tools/w3x2lni/bin/w3x2lni-lua.exe ${dir}/tools/语法检查.lua ${dir}
+	    		;;
+	    	obj)
+				${dir}/tools/w3x2lni/w2l.exe obj ${dir} ${dir}/${map_name}
+	    		;;
+	    	lni)
+				${dir}/tools/w3x2lni/w2l.exe lni ${dir}/${map_name} ${dir} 
+	    		;;
+	    	slk)
+				${dir}/tools/w3x2lni/w2l.exe slk ${dir} ${dir}/${map_name}
+	    		;;
+		esac
+	done
+}
+
+main "$@"
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # This is crazy monster base one war3
+
+# vim lua tags
+ctags.exe 5.8
+
+ctags.exe --langdef=MYLUA --langmap=MYLUA:.lua --regex-MYLUA="/^.*\s*function\s*(\w+):(\w+).*$/\2/f/" --regex-MYLUA="/^\s*(\w+)\s*=\s*[0-9]+.*$/\1/e/" --regex-MYLUA="/^.*\s*function\s*(\w+)\.(\w+).*$/\2/f/" --regex-MYLUA="/^.*\s*function\s*(\w+)\s*\(.*$/\1/f/" --regex-MYLUA="/^\s*(\w+)\s*=\s*\{.*$/\1/e/" --regex-MYLUA="/^\s*module\s+\"(\w+)\".*$/\1/m,module/" --regex-MYLUA="/^\s*module\s+\"[a-zA-Z0-9._]+\.(\w+)\".*$/\1/m,module/" --languages=MYLUA --excmd=number -R .
